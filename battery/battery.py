b726cce953014a280e9168b3bae07ec8cfa38469
brand95 <140763825+brand95@users.noreply.github.com>
2023-08-11 19:05:11 -0500

    refactored code

---
+++ battery/battery.py
@@ -0,0 +1,9 @@
+from abc import ABC
+
+
+class Battery(ABC):
+    def __init__(self):
+        pass
+
+    def needs_service(self) -> bool:
+        pass
---
+++ battery/nubbin_battery.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from battery.battery import Battery
+
+
+class NubbinBattery(Battery):
+    def __init__(self, current_date, last_service_date):
+        super().__init__()
+        self.current_date = current_date
+        self.last_service_date = last_service_date
+
+    def needs_service(self) -> bool:
+        current_date = datetime.today().date()
+        return (current_date - self.last_service_date).days >= 1460  # 4 years
